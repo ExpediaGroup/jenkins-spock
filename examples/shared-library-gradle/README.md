c40560622ffec62518fea7abb0a6476b6be00dae
Austin Witt <awitt@homeaway.com>
2020-01-15 14:17:03 -0600

    taking a stab at a Gradle build

---
+++ examples/shared-library-gradle/README.md
@@ -0,0 +1,49 @@
+Pipeline Shared Library
+==============================
+
+This project is a Jenkins Pipeline Shared Library that could be loaded by the [Pipeline Shared Groovy Libraries Plugin](https://plugins.jenkins.io/workflow-cps-global-lib).
+
+It delivers two new pipeline steps:
+
+`DefaultPipeline`
+-------------------------
+
+A simple Jenkins pipeline that 
+
+1. Builds & Tests all branches
+2. Notifies Slack if tests fail
+3. Deploys the "master" branch to the Production environment
+4. Deploys other branches to the "Test" environment.
+
+Usage:
+
+`Jenkinsfile`:
+
+```groovy
+@Library("shared-library") _
+DefaultPipeline()
+```
+
+`Deployer`
+-------------------------
+
+A step that SSHs to a machine in an environment, and bounces an application using `docker-compose`.
+
+Usage:
+
+```
+Deployer("test") // deploy the latest version of the application to the TEST environment
+```
+
+Testing
+==============================
+
+To test the library, run `make test`.
+
+Requirements
+==============================
+
+The following tools should be installed in order to work with this project:
+
+1. `make`
+2. `java` 1.8+
---
+++ examples/shared-library-gradle/test/DefaultPipelineSpec.groovy
@@ -0,0 +1,50 @@
+import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
+
+public class DefaultPipelineSpec extends JenkinsPipelineSpecification {
+
+	def DefaultPipeline = null
+
+	public static class DummyException extends RuntimeException {
+		public DummyException(String _message) { super( _message ); }
+	}
+
+	def setup() {
+		DefaultPipeline = loadPipelineScriptForTest("vars/DefaultPipeline.groovy")
+		DefaultPipeline.getBinding().setVariable( "scm", null )
+		getPipelineMock("libraryResource")(_) >> {
+			return "Dummy Message"
+		}
+	}
+
+	def "Slack is notified when tests fail" () {
+		setup:
+			getPipelineMock("sh")("docker run --entrypoint python whole-pipeline -m unittest discover") >> {
+				throw new DummyException("Dummy test failure")
+			}
+		when:
+			try {
+				DefaultPipeline()
+			} catch( DummyException e ) {}
+		then:
+			1 * getPipelineMock("slackSend")( _ as Map )
+	}
+
+	def "Attempts to deploy MASTER branch to PRODUCTION" () {
+		setup:
+			DefaultPipeline.getBinding().setVariable( "BRANCH_NAME", "master" )
+		when:
+			DefaultPipeline()
+		then:
+			1 * getPipelineMock("Deployer.call")("production")
+	}
+
+	def "Does NOT attempt to deploy non-MASTER branch PRODUCTION" () {
+		setup:
+			DefaultPipeline.getBinding().setVariable( "BRANCH_NAME", "develop" )
+		when:
+			DefaultPipeline()
+		then:
+			0 * getPipelineMock("Deployer.call")("production")
+	}
+}
+
